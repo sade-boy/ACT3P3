9b26778722750fc9cef8440d26b7af7fe001736e
sade-boy <sade_24@alu.uabcs.mx>
2026-04-16 10:29:43 -0700

    gato tictac

--- src/actividad3/Ventana.java
+++ src/actividad3/Ventana.java
@@ -3,86 +3,55 @@ package actividad3;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.border.LineBorder;
 
 public class Ventana extends JFrame {
 
     public Ventana() {
-
-        this.setSize(600, 650);
+        this.setSize(600, 600);
         this.setLocationRelativeTo(null);
-        this.setTitle("Calculadora");
-        this.setMinimumSize(new Dimension(300,400));
-        this.setMaximumSize(new Dimension(700,700));
+        this.setMinimumSize(new Dimension(300,300));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.getContentPane().setBackground(Color.BLACK);
         this.setLayout(new BorderLayout());
+        this.setTitle("Juego del gato");
 
-        iniciarComponentes();
+        crearTablero();
 
         this.setVisible(true);
     }
 
-    public void iniciarComponentes() {
+    public void crearTablero() {
+        JPanel contenedor = new JPanel();
+        contenedor.setLayout(new GridLayout(3,3,10,10)); 
+        contenedor.setBackground(Color.WHITE);
 
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout(10,10));
-        mainPanel.setBackground(Color.WHITE);
-        mainPanel.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
-        this.add(mainPanel);
+        this.add(contenedor, BorderLayout.CENTER);
 
-       
-        JTextField display = new JTextField();
-        display.setFont(new Font("Arial", Font.BOLD, 48));
-        display.setHorizontalAlignment(JTextField.RIGHT);
-        mainPanel.add(display, BorderLayout.NORTH);
+        Font fuente = new Font("Inter", Font.BOLD, 60);
 
-        
-        JPanel centerPanel = new JPanel();
-        centerPanel.setLayout(new GridLayout(4,4,10,10));
-        centerPanel.setBackground(Color.yellow);
+        for(int i = 0; i < 9; i++){
+            JButton b = new JButton();
 
-     
-        centerPanel.add(new JButton("7"));
-        centerPanel.add(new JButton("8"));
-        centerPanel.add(new JButton("9"));
-        centerPanel.add(new JButton("/"));
-        centerPanel.add(new JButton("4"));
-        centerPanel.add(new JButton("5"));
-        centerPanel.add(new JButton("6"));
-        centerPanel.add(new JButton("*"));
-        centerPanel.add(new JButton("1"));
-        centerPanel.add(new JButton("2"));
-        centerPanel.add(new JButton("3"));
-        centerPanel.add(new JButton("-"));
-        centerPanel.add(new JButton("+/-"));
-        centerPanel.add(new JButton("0"));
-        centerPanel.add(new JButton("."));
-        centerPanel.add(new JButton("+"));
+            b.setFont(fuente);
+            b.setBackground(new Color(218, 230, 225));
+            b.setForeground(Color.WHITE);
+            b.setBorder(new LineBorder(new Color(0,180,216),2));
+            b.setFocusPainted(false);
 
-        mainPanel.add(centerPanel, BorderLayout.CENTER);
+            
+            b.setText(""); 
 
-        // PANEL INFERIOR
-        JPanel southPanel = new JPanel();
-        southPanel.setLayout(new FlowLayout());
-        southPanel.setBackground(Color.GREEN);
+            contenedor.add(b);
+        }
+    }
 
-        southPanel.add(new JButton("AC"));
-        southPanel.add(new JButton("%"));
-        southPanel.add(new JButton("delete"));
-        southPanel.add(new JButton("="));
-
-        mainPanel.add(southPanel, BorderLayout.SOUTH);
+    public static void main(String[] args) {
+        new Ventana();
     }
 }
-
-		
-	
